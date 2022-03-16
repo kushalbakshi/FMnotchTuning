d993d8ebb2008d5fe66be8f01ce7ced0c9f420de
kushalbakshi <kushalbakshi@tamu.edu>
2022-03-16 16:34:22 -0500

    Updated annotations + README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-While the code in this repository serves as basic importing, filtering, and plotting of microelectrode array data, the main purpose is to practice git and version control 
+
+
+While the code in this repository serves as basic importing, filtering, and plotting of microelectrode array data, the main purpose is to practice git and version control
+
+The 'notchTune' jupyter file is a standalone prototyping file whereas the .py files are in functional code format to allow integration into a larger coding package with ease. Both provide the same results.
+
+The notchRaster file uses spike times in a csv file to generate the rasters whereas NotchRasterMUA imports .pl2 files (Plexon-acquired electrophysiology data), filters for spikes, and subsequently creates and saves rasters.
--- notchRaster.py
+++ notchRaster.py
@@ -5,9 +5,19 @@ import os
 
 
 def plot_notch_raster(filelocation, bat, site, channel, poststim_window, savelocation):
+    """
+    Imports spike times from a csv/txt file and creates event raster plots.
+    filelocation: absolute path of the spike times file.
+    bat: Animal ID within filelocation subfolders
+    site: Site in auditory cortex
+    channel: Electrode channel being analyzed
+    poststim_window: Length of post stimulus window to include in the raster plot
+    savelocation: Absolute path of where the raster should be saved
+    """
     sr = 40000
     poststim_window *= sr
 
+    # Catches exception when some spike sorted files have a header and others do not
     try:
         spikes = np.loadtxt(filelocation+bat+'/Spiketimes/{0}_FMnotch_chn'.format(site)+str(channel)+'_times.txt',
                             delimiter=',')
@@ -16,11 +26,12 @@ def plot_notch_raster(filelocation, bat, site, channel, poststim_window, saveloc
                             delimiter=',', skiprows=1)
     spikes = spikes[:, 0] * sr
 
+    # Import stimulus timestamps
     ts = np.asarray(loadmat(filelocation+bat+'/Matfile/{0}_FMnotch/event.mat'.format(site)).get('ts'))
     ts *= sr
 
+    # Import stimulus identity and other relevant information
     marker = np.asarray(loadmat(filelocation+bat+'/Data/{0}_{1}_marker_fm.mat'.format(bat, site)).get('marker_fm'))
-
     notch_freq = np.unique(marker)
     trial_num = len(marker)/len(notch_freq)
 
@@ -33,6 +44,7 @@ def plot_notch_raster(filelocation, bat, site, channel, poststim_window, saveloc
     raster_mat = np.append(raster_mat, marker, axis=1)
     raster_mat = raster_mat[raster_mat[:, -1].argsort()]
 
+    # Initiate and create figure
     fig1, axs = plt.subplots(5, 2, figsize=(7, 10), sharey=True, sharex=True)
     params = {'xtick.labelsize': 12, 'ytick.labelsize': 12, 'axes.labelsize': 14, 'figure.titlesize': 18,
               'figure.titleweight': 'bold', 'lines.linewidth': 3}
@@ -82,7 +94,7 @@ def plot_notch_raster(filelocation, bat, site, channel, poststim_window, saveloc
     plt.savefig(savelocation+bat+'/Site_{0}_Chn{1}_FMnotch.jpg'.format(site, channel), dpi=300)
     plt.close()
 
-
+# Example
 for site in range(5, 7):
     for channel in range(1, 33):
         try:
